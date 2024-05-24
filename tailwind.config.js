7b34f0482a6f783676509d0a7ae8ed4027c64776
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2024-05-24 14:09:37 +0300

    xxzaxa

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'banner' : "url('/public/image/hero/hero.png')"
+        'banner' : "url('/public/image/hero/hero.png')",
+      },
+      backgroundImage: {
+        'bannertwo' : "url('/public/image/decont/banner2.png')",
       },
       colors: {
         'pink': 'rgba(187, 38, 73, 0.5)',
+      },
+      colors: {
+        'onecart': 'linear-gradient(180deg, rgba(217, 217, 217, 0.04) 12.5% , rgba(115, 115, 115, 0.02) 98%)'
       },
     },
   },
