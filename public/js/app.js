bfbae1a1419b8d9bdacba05afcca19745f992a39
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2024-05-31 08:08:07 +0300

    x

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,29 @@
 var swiper = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
+  cssMode: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// let menu = document.querySelector('#menu')
+// let close = document.querySelector('#close')
+// let enter = document.querySelector('#enter')
+
+// menu.addEventListener('click' , function(){
+//   enter.classList.add(enter)
+// })
