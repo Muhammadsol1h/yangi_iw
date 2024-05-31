ec3dc6834596f9fad30a7f14e34136228655122b
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2024-05-31 14:01:43 +0300

    x

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
   cssMode: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
   },
   pagination: {
     el: ".swiper-pagination",
@@ -19,6 +19,18 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+let resposive = document.querySelector('#responsive-menu')
+let hamburger_menu = document.querySelector('#hamburger-menu')
+let close_menu = document.querySelector('#close__menu')
+
+hamburger_menu.addEventListener('click' , function(){
+    resposive.classList.add('right-0')  
+    resposive.classList.remove('-right-full')
+})
+close_menu.addEventListener('click' , function(){
+  resposive.classList.add('-right-full')
+  resposive.classList.remove('right-0')
+})
 
 // let menu = document.querySelector('#menu')
 // let close = document.querySelector('#close')
