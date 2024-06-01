2bcad479291ede16d1fe4ce86f495e5ffab98358
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2024-06-01 23:46:54 +0300

    z

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+// ================================================ SWIPER ================================================
 var swiper = new Swiper(".mySwiper", {
   cssMode: true,
   navigation: {
@@ -19,23 +20,37 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
-let resposive = document.querySelector('#responsive-menu')
-let hamburger_menu = document.querySelector('#hamburger-menu')
-let close_menu = document.querySelector('#close__menu')
+// ================================================ HEADER ================================================
+let resposive = document.querySelector("#responsive-menu");
+let hamburger_menu = document.querySelector("#hamburger-menu");
+let close_menu = document.querySelector("#close__menu");
 
-hamburger_menu.addEventListener('click' , function(){
-    resposive.classList.add('right-0')  
-    resposive.classList.remove('-right-full')
+hamburger_menu.addEventListener("click", function () {
+  resposive.classList.add("right-0");
+  resposive.classList.remove("-right-full");
+});
+close_menu.addEventListener("click", function () {
+  resposive.classList.add("-right-full");
+  resposive.classList.remove("right-0");
+});
+// ================================================ ARTICLES ================================================
+let active = document.querySelectorAll("#active1");
+let activebtn1 = document.querySelector("#activebtn1");
+let activebtn2 = document.querySelector("#activebtn2");
+let activebtn3 = document.querySelector("#activebtn3");
+let enterbtn = document.querySelector("#enterbtn");
+let enterbtn1 = document.querySelector("#enterbtn");
+let enterbtn2 = document.querySelector("#enterbtn");
+active.forEach(function (item, index) {
+  item.addEventListener("click", function () {
+    active.forEach(function (item, index) {
+      active[index].classList.remove("active1");
+    });
+    active[index].classList.add("active1");
+  });
+});
+enterbtn.addEventListener('click' , function(){
+  activebtn1.classList.toggle('hidden')
 })
-close_menu.addEventListener('click' , function(){
-  resposive.classList.add('-right-full')
-  resposive.classList.remove('right-0')
-})
 
-// let menu = document.querySelector('#menu')
-// let close = document.querySelector('#close')
-// let enter = document.querySelector('#enter')
-
-// menu.addEventListener('click' , function(){
-//   enter.classList.add(enter)
-// })
+// ================================================ CONTACTK ================================================
---
+++ public/js/contact.js
@@ -0,0 +1,8 @@
+function toggleFropdown() {
+    let dropdown = document.querySelector('#dropdownbutton #dropdown');
+    dropdown.classList.toggle("hidden")
+}
+function button() {
+    let dropdownone = document.querySelector('#btn #btn1');
+    dropdownone.classList.toggle("hidden")
+}
