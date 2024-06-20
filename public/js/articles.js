8109166c60741dbfc341bcfeb6415b4a6868530a
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2024-06-20 13:28:44 +0300

    muhajon

--- public/js/articles.js
+++ public/js/articles.js
@@ -13,3 +13,21 @@ tabs.forEach((item, index) => {
         all_contents[index].classList.remove("hidden")
     })
 })
+
+
+let resposive = document.querySelector("#responsive-menu");
+let hamburger_menu = document.querySelector("#hamburger-menu");
+let close_menu = document.querySelector("#close__menu");
+let body = document.querySelector('body')
+
+hamburger_menu.addEventListener("click", function () {
+  body.classList.add('overflow-hidden')
+  resposive.classList.add("right-0");
+  resposive.classList.remove("-right-full");
+});
+close_menu.addEventListener("click", function () {
+  resposive.classList.add("-right-full");
+  resposive.classList.remove("right-0");
+  body.classList.remove('overflow-hidden')
+
+});
